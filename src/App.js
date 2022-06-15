fa06fa65d2887319dfa8cbee25b7ae9794f5ff88
ruk91 <sajithkasn@gmail.com>
2022-06-15 11:56:50 +0530

    ✨ Add skeleton

--- src/App.js
+++ src/App.js
@@ -3,21 +3,78 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="box">
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+        <div class="skeleton">
+          <div class="skeleton-left flex1">
+            <div class="square"></div>
+          </div>
+          <div class="skeleton-right flex2">
+            <div class="line h17 w40 m10"></div>
+            <div class="line"></div>
+            <div class="line h8 w50"></div>
+            <div class="line  w75"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
