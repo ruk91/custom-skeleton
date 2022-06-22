234f4c7eb9199ef2815d2e3fd17cf6987c94b94a
ruk91 <sajithkasn@gmail.com>
2022-06-22 12:36:06 +0530

    💄 Add skeleton for program list

--- src/App.js
+++ src/App.js
@@ -3,75 +3,117 @@ import './App.css';
 
 function App() {
   return (
-    <div class="container">
-      <div class="box">
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
+    <div>
+      <div className="cover-box">
+        <div className="skeleton-cover">
+          <div className="skeleton-cover-left flex1">
+            <div className="cover-square"></div>
           </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
-          </div>
-        </div>
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
-          </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
-          </div>
-        </div>
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
-          </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
+          <div className="skeleton-cover-right flex2">
+            <div className="line-title h20 w50"></div>
+            <div className="line-description w75 mb25"></div>
+            <div className="line-description"></div>
+            <div className="line-description"></div>
+            <div className="line-description"></div>
+            <div className="line-description w50 mb50"></div>
           </div>
         </div>
       </div>
 
-      <div class="box">
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
-          </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
-          </div>
-        </div>
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
-          </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
+      <div className="container">
+        <div className="box">
+          <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
           </div>
         </div>
-        <div class="skeleton">
-          <div class="skeleton-left flex1">
-            <div class="square"></div>
+
+        <div className="box">
+          <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
           </div>
-          <div class="skeleton-right flex2">
-            <div class="line h17 w40 m10"></div>
-            <div class="line"></div>
-            <div class="line h8 w50"></div>
-            <div class="line  w75"></div>
+        </div>
+      </div>
+
+      <div className="cover-box">
+        <div className="skeleton-cover">
+          <div className="skeleton-cover-left flex1">
+            <div className="cover-square"></div>
+          </div>
+          <div className="skeleton-cover-right flex2">
+            <div className="line-title h20 w50"></div>
+            <div className="line-description w75 mb25"></div>
+            <div className="line-description"></div>
+            <div className="line-description"></div>
+            <div className="line-description"></div>
+            <div className="line-description w50 mb50"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="box">
+        <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
+          </div>
+          <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="box">
+        <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
+          </div>
+          <div className="skeleton">
+            <div className="skeleton-left flex1">
+              <div className="square"></div>
+            </div>
+            <div className="skeleton-right flex2">
+              <div className="line h17 w40 m10"></div>
+              <div className="line"></div>
+              <div className="line h8 w50"></div>
+              <div className="line  w75"></div>
+            </div>
           </div>
         </div>
       </div>
